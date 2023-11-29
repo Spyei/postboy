70a43ce2b25207c1ede599d943a8deb08b83f4e6
Spyei <caio.salamao1234@gmail.com>
2023-11-29 14:15:05 -0300

    fix: input width

--- src/app/Input.tsx
+++ src/app/Input.tsx
@@ -3,7 +3,7 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 export default function Input() {
     return (
         <div className="flex items-center justify-center">
-            <div className="bg-neutral-800 flex items-center rounded-lg p-3 h-16 w-screen gap-3 rounded-r-none">
+            <div className="bg-neutral-800 flex items-center rounded-lg p-3 h-16 w-full gap-3 rounded-r-none">
                 <button className="flex gap-3 items-center justify-start">
                     <span className="text-green-400">GET</span>
                     <MdKeyboardArrowDown fill="#ffffff" />
