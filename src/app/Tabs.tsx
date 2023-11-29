8c110419b81da91a911cc9aa65681f0219205bcb
Spyei <caio.salamao1234@gmail.com>
2023-11-29 14:14:55 -0300

    feat: tabs component

---
+++ src/app/Tabs.tsx
@@ -0,0 +1,41 @@
+"use client"
+import { useState } from "react";
+
+export default function Tabs({ tabs }: { tabs: { name: string; action?: (() => void) | (() => Promise<void>) }[] }) {
+    const [activeTab, setActiveTab] = useState<number>(1);
+
+    const handleTabClick = (index: number) => {
+        setActiveTab(index);
+    };
+
+    return (
+        <>
+            <div className="relative w-max h-12 grid grid-cols-3 items-center px-1 overflow-hidden transition text-white">
+                <div
+                    className="absolute h-11 my-auto top-0 left-0 rounded-lg bg-neutral-700 transition-transform"
+                    style={{
+                        width: "33%",
+                        transform: `translateX(${(activeTab - 1) * 100}%)`,
+                        transition: "transform 0.2s ease-in-out",
+                    }}
+                />
+                {tabs.map((tab, index) => (
+                    <button
+                        key={index}
+                        role="tab"
+                        aria-selected={activeTab === index + 1}
+                        tabIndex={activeTab === index + 1 ? 0 : -1}
+                        className="relative h-10 px-3 tab rounded-lg"
+                        onClick={() => {
+                            handleTabClick(index + 1);
+                            
+                            return tab.action;
+                        }}
+                    >
+                        <span>{tab.name}</span>
+                    </button>
+                ))}
+            </div>
+        </>
+    );
+};
